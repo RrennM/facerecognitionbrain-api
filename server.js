14270f415bded14eb911e66bcf63025ba1264a82
Tyson Hood <TysonHood@gmail.com>
2020-05-11 16:29:25 -0700

    Get /signin to function correctly (w notes).

--- server.js
+++ server.js
@@ -18,26 +18,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: 1,
-            name: 'Tyson',
-            email: 'TysonHood@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: 2,
-            name: 'Hank',
-            email: 'HankBirf@gmail.com',
-            password: 'biscuits',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
+// const database = {
+//     users: [
+//         {
+//             id: 1,
+//             name: 'Tyson',
+//             email: 'TysonHood@gmail.com',
+//             password: 'cookies',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: 2,
+//             name: 'Hank',
+//             email: 'HankBirf@gmail.com',
+//             password: 'biscuits',
+//             entries: 0,
+//             joined: new Date()
+//         }
+//     ]
+// }
 
 app.get('/', (req, res) => {
     res.send(database.users)
@@ -54,13 +54,31 @@ app.post('/signin', (req, res) => {
     //     console.log( 'second guess', res )
     // });
 
-    if( req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password ) {
-        // res.json('Success')
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('Error logging in.')
-    }
+    // if( req.body.email === database.users[0].email && 
+    //     req.body.password === database.users[0].password ) {
+    //     // res.json('Success')
+    //     res.json(database.users[0]);
+    // } else {
+    //     res.status(400).json('Error logging in.')
+    // }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            console.log(isValid)
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        console.log(user)
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json("Unable to get user."))
+            } else {
+                res.status(400).json('Wrong credentials.')
+              }
+        })
+        .catch(err => res.status(400).json('Wrong credentials.'))
 });
 
 app.post('/register', (req, res) => {
